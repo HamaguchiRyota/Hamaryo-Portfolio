034608387ba7fee750bcc17cad4c5ca68fbe8f5c
Jamix <52881627+Fumolat@users.noreply.github.com>
2024-03-06 18:35:25 +0900

    一部書き換え

--- theme.config.js
+++ theme.config.js
@@ -3,7 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Your Name.
+      <time>{YEAR}</time> © Hamaryo.
       <a href="/feed.xml">RSS</a>
       <style jsx>{`
         a {
@@ -17,5 +17,7 @@ export default {
         }
       `}</style>
     </small>
-  )
+
+  ),
+  darkMode: true,
 }
